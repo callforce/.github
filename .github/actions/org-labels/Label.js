e59b85ad0e0677060f9824f124a7bcff73df2a35
Misha Milovidov <mishamilovidov@icloud.com>
2020-01-19 08:49:26 -0800

    Update Label methods

--- .github/actions/org-labels/Label.js
+++ .github/actions/org-labels/Label.js
@@ -4,46 +4,58 @@ class Label {
     this.octokit = octokit
   }
 
-  async create({ owner, repo, name, color, description }) {
-    try {
-      await this.octokit.issues.createLabel({ 
-        owner, 
-        repo, 
-        name, 
-        color, 
-        description 
-      })
-      console.log(`Created ${name} label in ${repo}`)
-    } catch (err) {
-      console.log(`Failed to create ${name} label in ${repo}`)
-      console.log(err)
-    }
+  create({ owner, repo, name, color, description }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await this.octokit.issues.createLabel({ 
+          owner, 
+          repo, 
+          name, 
+          color, 
+          description 
+        })
+        console.log(`Created ${name} label in ${repo}`)
+        resolve({ name })
+      } catch (err) {
+        console.log(`Failed to create ${name} label in ${repo}`)
+        console.log(err)
+        reject(err)
+      }
+    })
   }
 
-  async delete({ owner, repo, name }) {
-    try {
-      await this.octokit.issues.deleteLabel({ owner, repo, name })
-      console.log(`Deleted ${name} label in ${repo}`)
-    } catch (err) {
-      console.log(`Failed to delete ${name} label in ${repo}`)
-      console.log(err)
-    }
+  delete({ owner, repo, name }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await this.octokit.issues.deleteLabel({ owner, repo, name })
+        console.log(`Deleted ${name} label in ${repo}`)
+        resolve({ name })
+      } catch (err) {
+        console.log(`Failed to delete ${name} label in ${repo}`)
+        console.log(err)
+        reject(err)
+      }
+    })
   }
 
-  async update({ owner, repo, name, color, description }) {
-    try {
-      await this.octokit.issues.updateLabel({ 
-        owner, 
-        repo, 
-        current_name: name, 
-        color, 
-        description 
-      })
-      console.log(`Updated ${name} label in ${repo}`)
-    } catch (err) {
-      console.log(`Failed to update ${name} label in ${repo}`)
-      console.log(err)
-    }
+  update({ owner, repo, name, color, description }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await this.octokit.issues.updateLabel({ 
+          owner, 
+          repo, 
+          current_name: name, 
+          color, 
+          description 
+        })
+        console.log(`Updated ${name} label in ${repo}`)
+        resolve({ name })
+      } catch (err) {
+        console.log(`Failed to update ${name} label in ${repo}`)
+        console.log(err)
+        reject(err)
+      }
+    })
   }
 }
 
