5701363e49bc5d892ab1585afa7b86d4b21514af
Misha Milovidov <mishamilovidov@icloud.com>
2020-01-19 20:45:11 -0800

    Move action to src directory

--- .github/actions/org-labels/dist/index.js
+++ .github/actions/org-labels/dist/index.js
@@ -34,7 +34,7 @@ module.exports =
 /******/ 	// the startup function
 /******/ 	function startup() {
 /******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(104);
+/******/ 		return __webpack_require__(676);
 /******/ 	};
 /******/ 	// initialize runtime
 /******/ 	runtime(__webpack_require__);
@@ -490,173 +490,6 @@ module.exports = windowsRelease;
 /***/ (function(module) {
 
 module.exports = require("os");
-
-/***/ }),
-
-/***/ 104:
-/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
-
-const _ = __webpack_require__(557)
-const core = __webpack_require__(470)
-const Label = __webpack_require__(640)
-const Oktokit = __webpack_require__(0)
-const { retry } = __webpack_require__(755)
-const { throttling } = __webpack_require__(617)
-
-/** 
- * @constant
- * @type {Object[]}
- * @default
-*/
-const PROTECTED_LABELS = ['Epic']
-
-/**
- * @function 
- * @name updateRepoLabels 
- *    updates repo labels using org labels config
- *      labels not in org label config are deleted
- *      labels missing in repo are created
- *      labels included in org label config are updated
- * 
- * @param {Octokit} octokit - Authenticated instance of Octokit
- * @param {String} org - Name of github org
- * @param {Object[]} repo - Org repo data
- * @param {Object[]} labels - Collection of org labels from org label config
- */
-const updateRepoLabels = (octokit, org, repo, labels) => {
-  return new Promise(async (resolve, reject) => {
-    let promises = []
-
-    try {
-      const orgLabel = new Label({ octokit })
-      const { data: repoLabels } = await octokit.issues.listLabelsForRepo({
-        owner: org,
-        repo: repo.name
-      })
-      const labelNames = _.map(labels, 'name')
-      const repoLabelNames = _.map(repoLabels, 'name')
-
-      repoLabels.forEach(label => {
-        if (
-          !labelNames.includes(label.name) && 
-          !PROTECTED_LABELS.includes(label.name)
-        ) {
-          promises.push(
-            orgLabel.delete({ owner: org, repo: repo.name, name: label.name })
-          )
-        }
-
-        if (
-          labelNames.includes(label.name) && 
-          !PROTECTED_LABELS.includes(label.name)
-        ) {
-          const { name, color, description } = _.mapKeys(
-            labels, 
-            'name'
-          )[label.name]
-
-          promises.push(
-            orgLabel.update({ owner: org, repo: repo.name, name, color, description })
-          )
-        }
-      })
-
-      labels.forEach(label => {
-        const { name, color, description } = label
-
-        if (!repoLabelNames.includes(label.name)) {
-          promises.push(
-            orgLabel.create({ owner: org, repo: repo.name, name, color, description })
-          )
-        }
-      })
-
-      let r = await Promise.all(promises)
-
-      resolve(r)
-    } catch (err) {
-      reject(err)
-    }
-  })
-}
-
-/**
- * @function 
- * @name standardizeLabels 
- *    creates and executes array of promises to update labels in org repos
- * 
- * @param {Octokit} octokit - Authenticated instance of Octokit
- * @param {String} org - Name of github org
- * @param {Object[]} repos - Collection of org repo data
- * @param {Object[]} labels - Collection of org labels from org label config
- */
-const standardizeLabels = (octokit, org, repos, labels) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      let promises = []
-
-      repos.forEach(repo => {
-        promises.push(updateRepoLabels(octokit, org, repo, labels))
-      })
-
-      let r = await Promise.all(promises)
-      
-      resolve(r)
-    } catch (err) {
-      reject(err)
-    }
-  })
-}
-
-/**
- * @function 
- * @name main
- *    gathers action inputs, github org repos, and org labels config file and 
- *    then standardizes labels in repos across the org
- */
-const main = async () => {
-  try {
-    const org = core.getInput('org', { required: true })
-    const repo = core.getInput('repo', { required: true })
-    const token = core.getInput('token', { required: true })
-    const labelsPath = core.getInput('labelsPath', { required: true })
-    const ActionOctokit = Oktokit.plugin(throttling).plugin(retry)
-    const octokit = new ActionOctokit({ 
-      auth: token,
-      throttle: {
-        onRateLimit: (retryAfter, options) => {
-          octokit.log.warn(
-            `Request quota exhausted for request ${options.method} ${options.url}`
-          )
-    
-          if (options.request.retryCount === 0) {
-            console.log(`Retrying after ${retryAfter} seconds!`)
-            return true
-          }
-        },
-        onAbuseLimit: (retryAfter, options) => {
-          octokit.log.warn(
-            `Abuse detected for request ${options.method} ${options.url}`
-          )
-        }
-      }
-    })
-    const { data: repos } = await octokit.repos.listForOrg({ org })
-    const { data: labelsConfig } = await octokit.repos.getContents({
-      owner: org,
-      repo,
-      path: labelsPath
-    })
-    const buff = new Buffer.from(labelsConfig.content, 'base64')
-    const labels = JSON.parse(buff.toString('utf-8'))
-    
-    await standardizeLabels(octokit, org, repos, labels)
-  } catch (err) {
-    core.setFailed(err.message)
-  }
-}
-
-main()
 
 /***/ }),
 
@@ -2072,6 +1905,107 @@ function applyAcceptHeader (res, headers) {
 
   return headers
 }
+
+
+/***/ }),
+
+/***/ 271:
+/***/ (function(module) {
+
+
+/**
+ * Creates interface to manage github issue label
+ * @class
+ */
+class Label {
+  constructor({ octokit }) {
+    this.octokit = octokit;
+  }
+
+  /**
+   * @method
+   * @name create
+   *    creates a github repo label
+   *
+   * @param {String} owner - Name of github org
+   * @param {String} repo - Org repo name
+   * @param {String} name - Label name
+   * @param {String} color - Label color; six-character hex code
+   * @param {String} description - Label description
+   */
+  async create({
+    owner, repo, name, color, description,
+  }) {
+    try {
+      await this.octokit.issues.createLabel({
+        owner,
+        repo,
+        name,
+        color,
+        description,
+      });
+      console.info(`Created ${name} label in ${repo}`);
+      return name;
+    } catch (err) {
+      console.info(`Failed to create ${name} label in ${repo}`);
+      return err;
+    }
+  }
+
+  /**
+   * @method
+   * @name delete
+   *    deletes a github repo label
+   *
+   * @param {String} owner - Name of github org
+   * @param {String} repo - Org repo name
+   * @param {String} name - Label name
+   */
+  async delete({ owner, repo, name }) {
+    try {
+      await this.octokit.issues.deleteLabel({ owner, repo, name });
+      console.info(`Deleted ${name} label in ${repo}`);
+      return name;
+    } catch (err) {
+      console.info(`Failed to delete ${name} label in ${repo}`);
+      return err;
+    }
+  }
+
+  /**
+   * @method
+   * @name update
+   *    creates a github repo label
+   *
+   * @param {String} owner - Name of github org
+   * @param {String} repo - Org repo name
+   * @param {String} name - Label name
+   * @param {String} color - Label color; six-character hex code
+   * @param {String} description - Label description
+   */
+  async update({
+    owner, repo, name, color, description,
+  }) {
+    try {
+      await this.octokit.issues.updateLabel({
+        owner,
+        repo,
+        name,
+        current_name: name,
+        new_name: name,
+        color,
+        description,
+      });
+      console.info(`Updated ${name} label in ${repo}`);
+      return name;
+    } catch (err) {
+      console.info(`Failed to update ${name} label in ${repo}`);
+      return err;
+    }
+  }
+}
+
+module.exports = Label;
 
 
 /***/ }),
@@ -24868,108 +24802,6 @@ module.exports = require("path");
 
 /***/ }),
 
-/***/ 640:
-/***/ (function(module) {
-
-
-/**
- * Creates interface to manage github issue label
- * @class
- */
-class Label {
-  constructor({ octokit }) {
-    this.octokit = octokit
-  }
-
-  /**
-   * @method 
-   * @name create 
-   *    creates a github repo label
-   * 
-   * @param {String} owner - Name of github org
-   * @param {String} repo - Org repo name
-   * @param {String} name - Label name
-   * @param {String} color - Label color; six-character hex code
-   * @param {String} description - Label description
-   */
-  create({ owner, repo, name, color, description }) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await this.octokit.issues.createLabel({ 
-          owner, 
-          repo, 
-          name, 
-          color, 
-          description 
-        })
-        console.log(`Created ${name} label in ${repo}`)
-        resolve({ name })
-      } catch (err) {
-        console.log(`Failed to create ${name} label in ${repo}`)
-        reject(err)
-      }
-    })
-  }
-
-  /**
-   * @method 
-   * @name delete 
-   *    deletes a github repo label
-   * 
-   * @param {String} owner - Name of github org
-   * @param {String} repo - Org repo name
-   * @param {String} name - Label name
-   */
-  delete({ owner, repo, name }) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await this.octokit.issues.deleteLabel({ owner, repo, name })
-        console.log(`Deleted ${name} label in ${repo}`)
-        resolve({ name })
-      } catch (err) {
-        console.log(`Failed to delete ${name} label in ${repo}`)
-        reject(err)
-      }
-    })
-  }
-
-  /**
-   * @method 
-   * @name update 
-   *    creates a github repo label
-   * 
-   * @param {String} owner - Name of github org
-   * @param {String} repo - Org repo name
-   * @param {String} name - Label name
-   * @param {String} color - Label color; six-character hex code
-   * @param {String} description - Label description
-   */
-  update({ owner, repo, name, color, description }) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await this.octokit.issues.updateLabel({
-          owner,
-          repo,
-          name,
-          current_name: name,
-          new_name: name,
-          color,
-          description
-        })
-        console.log(`Updated ${name} label in ${repo}`)
-        resolve({ name })
-      } catch (err) {
-        console.log(`Failed to update ${name} label in ${repo}`)
-        reject(err)
-      }
-    })
-  }
-}
-
-module.exports = Label
-
-/***/ }),
-
 /***/ 649:
 /***/ (function(module, __unusedexports, __webpack_require__) {
 
@@ -25116,6 +24948,180 @@ function authenticate(state, options) {
 module.exports = function btoa(str) {
   return new Buffer(str).toString('base64')
 }
+
+
+/***/ }),
+
+/***/ 676:
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+
+const _ = __webpack_require__(557);
+const core = __webpack_require__(470);
+const Oktokit = __webpack_require__(0);
+const { retry } = __webpack_require__(755);
+const { throttling } = __webpack_require__(617);
+const Label = __webpack_require__(271);
+
+/**
+ * @constant
+ * @type {Object[]}
+ * @default
+*/
+const PROTECTED_LABELS = ['Epic'];
+
+/**
+ * @function
+ * @name updateRepoLabels
+ *    updates repo labels using org labels config
+ *      labels not in org label config are deleted
+ *      labels missing in repo are created
+ *      labels included in org label config are updated
+ *
+ * @param {Octokit} octokit - Authenticated instance of Octokit
+ * @param {String} org - Name of github org
+ * @param {Object[]} repo - Org repo data
+ * @param {Object[]} labels - Collection of org labels from org label config
+ */
+async function updateRepoLabels(octokit, org, repo, labels) {
+  const promises = [];
+
+  try {
+    const orgLabel = new Label({ octokit });
+    const { data: repoLabels } = await octokit.issues.listLabelsForRepo({
+      owner: org,
+      repo: repo.name,
+    });
+    const labelNames = _.map(labels, 'name');
+    const repoLabelNames = _.map(repoLabels, 'name');
+
+    repoLabels.forEach((label) => {
+      if (
+        !labelNames.includes(label.name)
+        && !PROTECTED_LABELS.includes(label.name)
+      ) {
+        promises.push(
+          orgLabel.delete({ owner: org, repo: repo.name, name: label.name }),
+        );
+      }
+
+      if (
+        labelNames.includes(label.name)
+        && !PROTECTED_LABELS.includes(label.name)
+      ) {
+        const { name, color, description } = _.mapKeys(
+          labels,
+          'name',
+        )[label.name];
+
+        promises.push(
+          orgLabel.update({
+            owner: org, repo: repo.name, name, color, description,
+          }),
+        );
+      }
+    });
+
+    labels.forEach((label) => {
+      const { name, color, description } = label;
+
+      if (!repoLabelNames.includes(label.name)) {
+        promises.push(
+          orgLabel.create({
+            owner: org, repo: repo.name, name, color, description,
+          }),
+        );
+      }
+    });
+
+    const r = await Promise.all(promises);
+
+    return r;
+  } catch (err) {
+    return err;
+  }
+}
+
+/**
+ * @function
+ * @name standardizeLabels
+ *    creates and executes array of promises to update labels in org repos
+ *
+ * @param {Octokit} octokit - Authenticated instance of Octokit
+ * @param {String} org - Name of github org
+ * @param {Object[]} repos - Collection of org repo data
+ * @param {Object[]} labels - Collection of org labels from org label config
+ */
+async function standardizeLabels(octokit, org, repos, labels) {
+  try {
+    const promises = [];
+
+    repos.forEach((repo) => {
+      promises.push(updateRepoLabels(octokit, org, repo, labels));
+    });
+
+    const r = await Promise.all(promises);
+
+    return r;
+  } catch (err) {
+    return err;
+  }
+}
+
+/**
+ * @function
+ * @name main
+ *    gathers action inputs, github org repos, and org labels config file and
+ *    then standardizes labels in repos across the org
+ */
+async function main() {
+  try {
+    // const org = core.getInput('org', { required: true })
+    // const repo = core.getInput('repo', { required: true })
+    // const token = core.getInput('token', { required: true })
+    // const labelsPath = core.getInput('labelsPath', { required: true })
+    const org = 'callforce';
+    const repo = '.github';
+    const token = '86409a23020af679206c60f55df7b4c10fb3d10a';
+    const labelsPath = 'config/org-labels.json';
+    const ActionOctokit = Oktokit.plugin(throttling).plugin(retry);
+    const octokit = new ActionOctokit({
+      auth: token,
+      throttle: {
+        onRateLimit: (retryAfter, options) => {
+          octokit.log.warn(
+            `Request quota exhausted for request ${options.method} ${options.url}`,
+          );
+
+          if (options.request.retryCount === 0) {
+            console.log(`Retrying after ${retryAfter} seconds!`);
+            return true;
+          }
+
+          return null;
+        },
+        onAbuseLimit: (retryAfter, options) => {
+          octokit.log.warn(
+            `Abuse detected for request ${options.method} ${options.url}`,
+          );
+        },
+      },
+    });
+    const { data: repos } = await octokit.repos.listForOrg({ org });
+    const { data: labelsConfig } = await octokit.repos.getContents({
+      owner: org,
+      repo,
+      path: labelsPath,
+    });
+    const buff = Buffer.from(labelsConfig.content, 'base64');
+    const labels = JSON.parse(buff.toString('utf-8'));
+
+    await standardizeLabels(octokit, org, repos, labels);
+  } catch (err) {
+    core.setFailed(err.message);
+  }
+}
+
+main();
 
 
 /***/ }),
